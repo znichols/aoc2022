85a717e176dc2caaa49c25a7549afb50c3f9c262
Zack Nichols <zachary.nichols@gmail.com>
2022-12-01 10:53:52 -0500

    Set up rustfmt

--- src/days/day01/main.rs
+++ src/days/day01/main.rs
@@ -2,15 +2,14 @@ use std::env;
 use std::error::Error;
 use std::fs;
 
-
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     let input = fs::read_to_string(&args[1])?;
 
-    let mut elf_sums: Vec<i32> = input.split("\n\n")
-        .map(|l| {
-            l.split('\n').flat_map(|n| n.parse::<i32>()).sum()
-        }).collect();
+    let mut elf_sums: Vec<i32> = input
+        .split("\n\n")
+        .map(|l| l.split('\n').flat_map(|n| n.parse::<i32>()).sum())
+        .collect();
 
     elf_sums.sort_by(|a, b| b.cmp(a));
 
